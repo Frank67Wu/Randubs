1848076efb57ecea265b081ab79375c483b2dfb3
Frank67Wu <f67wu@uwaterloo.ca>
2024-02-20 20:10:19 -0800

    First commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,251 @@
-import Image from "next/image";
+'use client'
+
+import { Box, TextField, Typography, Button } from "@mui/material";
+import { useState, useEffect } from "react";
+import QuestionMarkIcon from '@mui/icons-material/QuestionMark';
+
 
 export default function Home() {
+
+  const red = "#F52E2E"
+  const blue = "#5464FF"
+  const purple = 	"#FFD6D7"
+
+  const [playerOne, setPlayerOne] = useState(window?.localStorage?.getItem("p1") || "Player One")
+  const [playerTwo, setPlayerTwo] = useState(window?.localStorage?.getItem("p2") || "Player Two")
+  const [playerThree, setPlayerThree] = useState(window?.localStorage?.getItem("p3") || "Player Three")
+  const [playerFour, setPlayerFour] = useState(window?.localStorage?.getItem("p4") || "Player Four")
+  const [matchAmount, setMatchAmount] = useState(window?.localStorage?.getItem("matchAmount") || 1)
+  const [playerColors, setPlayerColors] = useState([red, red, blue, blue])
+  const [total, setTotal] = useState(["0", "0", "0", "0"])
+  const [loading, setLoading] = useState(false)
+  const [cachingFinished, setCachingFinished] = useState(false)
+
+  useEffect(() => {
+    const previousTotal = [localStorage.getItem("p1total"), localStorage.getItem("p2total"), localStorage.getItem("p3total"), localStorage.getItem("p4total")]
+
+    if (previousTotal[0] !== 'null') {
+      setTotal(previousTotal)
+    }
+
+    setCachingFinished(true)
+  }, [])
+
+  useEffect(() => {
+    if (cachingFinished) {
+      localStorage.setItem("p1", playerOne)
+      localStorage.setItem("p2", playerTwo)
+      localStorage.setItem("p3", playerThree)
+      localStorage.setItem("p4", playerFour)
+    }
+  }, [playerOne, playerTwo, playerThree, playerFour])
+
+  useEffect(() => {
+    localStorage.setItem("p1total", total[0])
+    localStorage.setItem("p2total", total[1])
+    localStorage.setItem("p3total", total[2])
+    localStorage.setItem("p4total", total[3])
+  }, [total])
+
+  useEffect(() => {
+    if (cachingFinished) {
+      localStorage.setItem("matchAmount", matchAmount)
+    }
+  }, [matchAmount])
+
+
+
+  const randomizeTeams = () => {
+    setLoading(true)
+    let colors = [red, red, red, red]
+
+    let indexOne = Math.floor(Math.random() * (4))
+    let indexTwo = (Math.floor(Math.random() * (3)) + 1 + indexOne) % 4
+
+    colors[indexOne] = blue
+    colors[indexTwo] = blue
+
+    setPlayerColors(colors)
+
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1300)
+
+    return
+  }
+
+  const resetAll = () => {
+    localStorage.clear()
+    location.reload();
+  }
+
+  const updateTotal = (winner) => {
+    console.log("ASDFASDF")
+    let newTotal = []
+    for (let i = 0; i < 4; i++) {
+      console.log(playerColors[i] == winner)
+      if (playerColors[i] == winner) {
+        newTotal.push(parseInt(total[i]) + matchAmount)
+        console.log(parseInt(total[i]), matchAmount)
+      }
+      else {
+        newTotal.push(parseInt(total[i]) - matchAmount)
+      }
+    }
+
+    console.log(newTotal)
+    setTotal(newTotal)
+
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+      <Box sx={{ display: "flex", mt : "16px"}}>
+        <Typography variant="h6">Set Money Per Game : </Typography>
+        <TextField size="small" sx={{
+          "& input::-webkit-outer-spin-button, & input::-webkit-inner-spin-button": {
+            display: "none",
+          },
+          "& input[type=number]": {
+            MozAppearance: "textfield",
+          }, width : "40px", mt : "-4px", ml : "8px"
+        }} type="number" value={matchAmount} onChange={(e) => setMatchAmount(parseInt(e.target.value))}>{matchAmount}</TextField>
+      </Box>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <Button onClick={() => randomizeTeams()} sx={{ border: "solid", width: "8%", margin: "auto", height: "70px", mb : "64px"}}>
+        <QuestionMarkIcon></QuestionMarkIcon>
+      </Button>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+        <Box sx={{ display: "flex", flexDirection: "column", ml: "8px", mr: "8px" }}>
+          <TextField
+            sx={{
+              backgroundColor: playerColors[0] == blue ? loading ? purple : blue : loading ? purple : red,
+              transition: "all 1s ease",
+              WebkitTransition: "all 1s ease",
+              MozTransition: "all 1s ease"
+            }}
+            value={playerOne}
+            onChange={(e) => (setPlayerOne(e.target.value))}
+            InputProps={{
+              inputProps: {
+                style: { textAlign: "center" },
+              }
+            }} />
+          <Box sx={{
+            height: "170px", marginTop: "16px", borderRadius: "5px", background: playerColors[0], display: "flex", justifyContent: "center", alignItems: "center",
+            backgroundColor: playerColors[0] == blue ? loading ? purple : blue : loading ? purple : red,
+            transition: "all 1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"
+          }}>
+            <Typography variant="h2">{total[0]}</Typography>
+          </Box>
+        </Box>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Box sx={{ display: "flex", flexDirection: "column", ml: "8px", mr: "8px" }}>
+          <TextField
+            sx={{
+              backgroundColor: playerColors[1] == blue ? loading ? purple : blue : loading ? purple : red,
+              transition: "all 1s ease",
+              WebkitTransition: "all 1s ease",
+              MozTransition: "all 1s ease"
+            }}
+            value={playerTwo}
+            onChange={(e) => (setPlayerTwo(e.target.value))}
+            InputProps={{
+              inputProps: {
+                style: { textAlign: "center" },
+              }
+            }} />
+          <Box sx={{
+            height: "170px", marginTop: "16px", borderRadius: "5px", background: playerColors[1], display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: playerColors[1] == blue ? loading ? purple : blue : loading ? purple : red,
+            transition: "all 1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"
+          }}>
+            <Typography variant="h2">{total[1]}</Typography>
+          </Box>
+        </Box>
+
+        <Box sx={{ display: "flex", flexDirection: "column", ml: "8px", mr: "8px" }}>
+          <TextField
+            sx={{
+              backgroundColor: playerColors[2] == blue ? loading ? purple : blue : loading ? purple : red,
+              transition: "all 1s ease",
+              WebkitTransition: "all 1s ease",
+              MozTransition: "all 1s ease"
+            }}
+            value={playerThree}
+            onChange={(e) => (setPlayerThree(e.target.value))}
+            InputProps={{
+              inputProps: {
+                style: { textAlign: "center" },
+              }
+            }} />
+          <Box sx={{
+            height: "170px", marginTop: "16px", borderRadius: "5px", background: playerColors[2], display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: playerColors[2] == blue ? loading ? purple : blue : loading ? purple : red,
+            transition: "all 1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"
+          }}>
+            <Typography variant="h2">{total[2]}</Typography>
+          </Box>
+        </Box>
+
+        <Box sx={{ display: "flex", flexDirection: "column", ml: "8px", mr: "8px" }}>
+          <TextField
+            sx={{
+              backgroundColor: playerColors[3] == blue ? loading ? purple : blue : loading ? purple : red,
+              transition: "all 1s ease",
+              WebkitTransition: "all 1s ease",
+              MozTransition: "all 1s ease"
+            }}
+            value={playerFour}
+            onChange={(e) => (setPlayerFour(e.target.value))}
+            InputProps={{
+              inputProps: {
+                style: { textAlign: "center" },
+              }
+            }} />
+          <Box sx={{
+            height: "170px", marginTop: "16px", borderRadius: "5px", background: playerColors[3], display: "flex", justifyContent: "center", alignItems: "center", backgroundColor: playerColors[3] == blue ? loading ? purple : blue : loading ? purple : red,
+            transition: "all 1s ease",
+            WebkitTransition: "all 1s ease",
+            MozTransition: "all 1s ease"
+          }}>
+            <Typography variant="h2">{total[3]}</Typography>
+          </Box>
+        </Box>
+
+
+
+      </Box>
+
+
+      <Box sx={{ display: "flex", justifyContent: "center" }}>
+
+        <Button onClick={() => (updateTotal(red))} sx={{
+          "&:hover": {
+            backgroundColor: red,
+          }, background: red, color: "black", width: "150px", height: "120px", mr: "8px", mt: "16px"
+        }}>Red Win</Button>
+        <Button onClick={() => (updateTotal(blue))} sx={{
+          "&:hover": {
+            backgroundColor: blue,
+          }, background: blue, color: "black", width: "150px", height: "120px", ml: "8px", mt: "16px"
+        }}>Blue Win</Button>
+
+      </Box>
+
+
+      <Box> 
+        <Button onClick={() => (resetAll())} sx={{color : "black", border: "solid", bottom : 10, position : "fixed", width : "200px", paddingBottom : "20px", paddingTop : "20px"}}>Reset All</Button>
+      </Box>
+
+    </Box>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'smash-blue': "#F52E2E",
+        'smash-red' : "#5464FF",
+      },
     },
   },
   plugins: [],
