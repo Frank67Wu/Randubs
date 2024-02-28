d2809fe0325dd9abf196c36cd78ee14a27d8d650
Frank67Wu <f67wu@uwaterloo.ca>
2024-02-27 16:27:16 -0800

    add analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,16 @@
 import { grey } from "@mui/material/colors";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
   return (
     <html style={{height : "100%"}} lang="en">
-      <body style={{minHeight: "100vh" , position: "relative", background : "#343434", color : "white"}}>{children}</body>
+      <body style={{minHeight: "100vh" , position: "relative", background : "#343434", color : "white"}}>
+      
+      {children}
+      <Analytics />
+      
+      
+      </body>
     </html>
   );
 }
